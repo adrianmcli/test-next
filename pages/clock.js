0ad6af5e869dda3571a88fb6b1ad714519732571
Adrian Li <li.adrianmc@gmail.com>
2017-02-12 00:43:39 -0500

    temp fix for ssr

--- pages/clock.js
+++ pages/clock.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import Head from 'next/head'
-import ReduxProvider from '../modules/redux/ReduxProvider'
+
+import { Provider } from 'react-redux'
+import { reducers, initStore } from '../modules/redux'
+
 import ClockPage from '../modules/clock/Page'
 
-export default () =>
-  <div>
-    <Head>
-      <title>Clock Page</title>
-    </Head>
-    <ReduxProvider>
-      <ClockPage />
-    </ReduxProvider>
-  </div>
+export default class Component extends React.Component {
+  static getInitialProps ({ req }) {
+    const isServer = !!req
+    const store = initStore(reducers, { clock: null }, isServer)
+    store.dispatch({ type: 'TICK', light: !isServer, ts: Date.now() })
+    return { initialState: store.getState(), isServer }
+  }
+
+  constructor (props) {
+    super(props)
+    this.store = initStore(reducers, props.initialState, props.isServer)
+  }
+
+  render () {
+    return (
+      <div>
+        <Head>
+          <title>Clock Page</title>
+        </Head>
+        <Provider store={this.store}>
+          <ClockPage />
+        </Provider>
+    </div>
+    )
+  }
+}
