d5b4998ac702c372129545b92aa1ac24d88cc506
Adrian Li <li.adrianmc@gmail.com>
2017-02-11 22:31:43 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+
+import { reducer, initStore, startClock } from '../modules/redux'
+import Page from '../modules/clock/Page'
+
+export default class Counter extends React.Component {
+  static getInitialProps ({ req }) {
+    const isServer = !!req
+    const store = initStore(reducer, null, isServer)
+    store.dispatch({ type: 'TICK', light: !isServer, ts: Date.now() })
+    return { initialState: store.getState(), isServer }
+  }
+
+  constructor (props) {
+    super(props)
+    this.store = initStore(reducer, props.initialState, props.isServer)
+  }
+
+  componentDidMount () {
+    this.timer = this.store.dispatch(startClock())
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  render () {
+    return (
+      <Provider store={this.store}>
+        <Page title='Index Page' linkTo='/other' />
+      </Provider>
+    )
+  }
+}
