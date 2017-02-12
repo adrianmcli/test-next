1299cb88442930efa52e30e948a1138d68d73166
Adrian Li <li.adrianmc@gmail.com>
2017-02-11 22:48:17 -0500

    refactor wrapping redux class

--- modules/redux/index.js
+++ modules/redux/index.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 export const reducer = (state = { lastUpdate: 0, light: false }, action) => {
   switch (action.type) {
@@ -15,11 +16,12 @@ export const startClock = () => dispatch => {
 let store = null
 
 export const initStore = (reducer, initialState, isServer) => {
+  const enhancers = composeWithDevTools(applyMiddleware(thunkMiddleware))
   if (isServer && typeof window === 'undefined') {
-    return createStore(reducer, initialState, applyMiddleware(thunkMiddleware))
+    return createStore(reducer, initialState, enhancers)
   } else {
     if (!store) {
-      store = createStore(reducer, initialState, applyMiddleware(thunkMiddleware))
+      store = createStore(reducer, initialState, enhancers)
     }
     return store
   }
--- pages/other.js
+++ pages/other.js
@@ -1,35 +1,8 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-
-import { reducer, initStore, startClock } from '../modules/redux'
+import ReduxProvider from '../modules/redux/ReduxProvider'
 import Page from '../modules/clock/Page'
 
-export default class Counter extends React.Component {
-  static getInitialProps ({ req }) {
-    const isServer = !!req
-    const store = initStore(reducer, null, isServer)
-    store.dispatch({ type: 'TICK', light: !isServer, ts: Date.now() })
-    return { initialState: store.getState(), isServer }
-  }
-
-  constructor (props) {
-    super(props)
-    this.store = initStore(reducer, props.initialState, props.isServer)
-  }
-
-  componentDidMount () {
-    this.timer = this.store.dispatch(startClock())
-  }
-
-  componentWillUnmount () {
-    clearInterval(this.timer)
-  }
-
-  render () {
-    return (
-      <Provider store={this.store}>
-        <Page title='Other Page' linkTo='/' />
-      </Provider>
-    )
-  }
-}
+export default () =>
+  <ReduxProvider>
+    <Page title='Other Page' linkTo='/' />
+  </ReduxProvider>
